d879845277996de8c7ede931444ee3af541af631
Albert Snow <ajsnow2012@gmail.com>
2023-09-07 18:50:19 -0500

    fixing write populating read

--- src/main/java/com/example/receiptprocessor/services/receipt/Read.java
+++ src/main/java/com/example/receiptprocessor/services/receipt/Read.java
@@ -1,4 +1,17 @@
 package com.example.receiptprocessor.services.receipt;
 
+import com.example.receiptprocessor.data.repositories.ReceiptRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Read {
+
+	@Autowired
+	private final ReceiptRepository receiptRepository;
+
+	public Read(ReceiptRepository receiptRepository) {
+		this.receiptRepository = receiptRepository;
+	}
+
 }
--- src/main/java/com/example/receiptprocessor/services/receipt/Write.java
+++ src/main/java/com/example/receiptprocessor/services/receipt/Write.java
@@ -1,19 +1,20 @@
 package com.example.receiptprocessor.services.receipt;
 
-import com.example.receiptprocessor.data.repositories.Receipt;
+import com.example.receiptprocessor.data.repositories.ReceiptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class Write {
-	@Service
-	public class ReceiptService {
 		@Autowired
-		private Receipt receiptRepository;
+		private final ReceiptRepository receiptRepository;
+
+		public Write(ReceiptRepository receiptRepository) {
+			this.receiptRepository = receiptRepository;
+		}
 
 		public void saveReceipt(com.example.receiptprocessor.data.entities.Receipt receipt) {
 			// Implement logic to save the receipt data in the database
 			receiptRepository.save(receipt);
 		}
-	}
-
 }
