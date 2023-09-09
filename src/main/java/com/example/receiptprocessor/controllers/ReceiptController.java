f47297c245a4e4f40cb407a291722949fa70a323
Albert Snow <ajsnow2012@gmail.com>
2023-09-09 15:26:06 -0500

    becoming a mad scientist in real time

--- src/main/java/com/example/receiptprocessor/controllers/ReceiptController.java
+++ src/main/java/com/example/receiptprocessor/controllers/ReceiptController.java
@@ -1,5 +1,6 @@
 package com.example.receiptprocessor.controllers;
 
+import com.example.receiptprocessor.data.records.SimpleHTTPResponse;
 import com.example.receiptprocessor.data.states.Database;
 import com.example.receiptprocessor.data.states.Receipt;
 import com.example.receiptprocessor.services.receipt.ReceiptWrite;
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
@@ -26,19 +28,49 @@ public class ReceiptController {
 		this.receiptWrite = receiptService;
 	}
 
-	@PostMapping("/process")
-	public ResponseEntity<Map<String, String>> recordReceipt(@RequestBody JsonNode receipt) {
+	public SimpleHTTPResponse validateReceipt(@RequestBody JsonNode receipt) {
 		var jsonFile = Paths.get("src/main/java/com/example/receiptprocessor/data/schemas/receipt.json");
-		var validationResultOrFailure = Validation.validateJsonSchema(jsonFile, receipt);
+		var validationResultOrFailure = Validation.validateJsonSchema(jsonFile).apply(receipt);
 		var validationOptions = Receipt.processReceiptStatesOn(validationResultOrFailure);
 		var validationResult = Collections.firstTrueStateOf(validationOptions).get();
 		var validJson = validationResult.statusCode() == HttpStatus.CREATED;
 		var receiptEntity = validJson? ReceiptWrite.hydrate(receipt) : null;
 		var recordReceipt = Function0.of(() -> receiptWrite.recordReceipt(receiptEntity));
 		var success = validJson && recordReceipt.get() != null;
-		var responseResult = Collections.firstTrueEagerStateOf(List.of(
+		return Collections.firstTrueEagerStateOf(List.of(
 						Pair.of(success || !validJson, validationResult),
 						Pair.of(true, Database.couldNotWrite())));
+	}
+
+	public void validateItems(Path file, JsonNode items) {
+		var validateItem = Validation.validateJsonSchema(file);
+		for (JsonNode item : items) {
+			var validationResult = validateItem.apply(item);
+		}
+		// invalid json file
+	}
+
+	public SimpleHTTPResponse getItemValidationResult(@RequestBody JsonNode receipt) {
+		var jsonFile = Paths.get("src/main/java/com/example/receiptprocessor/data/schemas/items.json");
+		var items = receipt.get("items");
+		var validationResultsForItems = validateItems(jsonFile, items);
+//		var validationResultOrFailure = Validation.validateJsonSchema(jsonFile, receipt);
+		var validationOptions = Receipt.processReceiptStatesOn(validationResultOrFailure);
+		var validationResult = Collections.firstTrueStateOf(validationOptions).get();
+		var validJson = validationResult.statusCode() == HttpStatus.CREATED;
+		var receiptEntity = validJson? ReceiptWrite.hydrate(receipt) : null;
+		var recordReceipt = Function0.of(() -> receiptWrite.recordReceipt(receiptEntity));
+		var success = validJson && recordReceipt.get() != null;
+		return Collections.firstTrueEagerStateOf(List.of(
+						Pair.of(success || !validJson, validationResult),
+						Pair.of(true, Database.couldNotWrite())));
+	}
+
+	@PostMapping("/process")
+	public ResponseEntity<Map<String, String>> recordReceipt(@RequestBody JsonNode receipt) {
+		var receiptResponse = validateReceipt(receipt); // todo: get query to make, bundle w/ items
+		var itemResponses = getItemValidationResult(receipt); // todo: get query to make, bundle w/ receipt
+		// todo: try saving receipt and items, revert if either fail
 		return ResponseEntity.status(responseResult.statusCode()).body(responseResult.body());
 	}
 
--- src/main/java/com/example/receiptprocessor/utility/Validation.java
+++ src/main/java/com/example/receiptprocessor/utility/Validation.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.networknt.schema.JsonSchemaFactory;
 import com.networknt.schema.SpecVersion;
 import com.networknt.schema.ValidationMessage;
+import io.vavr.Function1;
 import io.vavr.control.Try;
 
 import java.nio.file.Files;
@@ -15,15 +16,19 @@ public class Validation {
 		throw new IllegalStateException("Utility class");
 	}
 
-	public static Try<Set<ValidationMessage>> validateJsonSchema(Path jsonFile, JsonNode jsonTree) {
-		// Read JSON from the file and map it to a Java object
-		JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
-		var maybeSchema = Try.of(() -> {
-			var schemaString = Files.readString(jsonFile);
-			return factory.getSchema(schemaString);
-		});
-		return maybeSchema.flatMap(schema ->
-						Try.success(schema.validate(jsonTree)));
-	}
-
+	public static Function1<JsonNode, Try<Set<ValidationMessage>>>
+	validateJsonSchema(Path jsonFile) {
+		return jsonTree -> {
+			// Read JSON from the file and map it to a Java object
+			JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4);
+			var maybeSchema = Try.of(() -> {
+				var schemaString = Files.readString(jsonFile);
+				return factory.getSchema(schemaString);
+			});
+			return maybeSchema.flatMap(schema ->
+							Try.success(schema.validate(jsonTree)));
+		};
+		// todo: this  function always have a finite number of outcomes
+		// could I just design this such that it returns a state map?
+		}
 }
