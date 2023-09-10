1f850017106b7bfae14b1f21bbf90a7a2647fd1d
Albert Snow <ajsnow2012@gmail.com>
2023-09-10 00:20:11 -0500

    updating process receipt to validate items

--- src/main/java/com/example/receiptprocessor/controllers/ReceiptController.java
+++ src/main/java/com/example/receiptprocessor/controllers/ReceiptController.java
@@ -1,14 +1,14 @@
 package com.example.receiptprocessor.controllers;
 
 import com.example.receiptprocessor.data.records.SimpleHTTPResponse;
-import com.example.receiptprocessor.data.states.Database;
 import com.example.receiptprocessor.data.states.Json;
-import com.example.receiptprocessor.data.states.Receipt;
+import com.example.receiptprocessor.services.item.ItemWrite;
 import com.example.receiptprocessor.services.receipt.ReceiptWrite;
 import com.example.receiptprocessor.utility.Collections;
 import com.example.receiptprocessor.utility.Validation;
 import com.fasterxml.jackson.databind.JsonNode;
-import io.vavr.Function0;
+import io.vavr.Lazy;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
@@ -19,60 +19,85 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/receipts")
 public class ReceiptController {
 	@Autowired
 	private final ReceiptWrite receiptWrite;
-	public ReceiptController(ReceiptWrite receiptService) {
+	@Autowired
+	private final ItemWrite itemWrite;
+	public ReceiptController(ReceiptWrite receiptService, ItemWrite itemWrite) {
 		this.receiptWrite = receiptService;
+		this.itemWrite = itemWrite;
 	}
 
-	public SimpleHTTPResponse validateReceipt(@RequestBody JsonNode receipt) {
+	Lazy<Object> getReceiptQuery(JsonNode receipt) {
+		var receiptEntity = ReceiptWrite.hydrate(receipt);
+		return Lazy.of(() -> { receiptWrite.recordReceipt(receiptEntity); return null; });
+	}
+
+	  List<Lazy<Object>> getItemQueries(@NotNull JsonNode items) {
+		return StreamSupport.stream(items.spliterator(), true)
+						.map(ItemWrite::hydrate)
+						.map(item -> Lazy.of(() -> { itemWrite.save(item); return null; }))
+						.toList();
+	}
+
+	public Pair<String, String> validateReceipt(@RequestBody JsonNode receipt) {
 		var jsonFile = Paths.get("src/main/java/com/example/receiptprocessor/data/schemas/receipt.json");
-		var validationOutcome = Validation.validateJsonSchemaFrom(jsonFile).apply(receipt).get();
-		var validationOutcomeState = validationOutcome.getFirst();
-		var validJson = validationOutcomeState.equals(Json.MATCHED_SCHEMA);
-		var receiptEntity = validJson? ReceiptWrite.hydrate(receipt) : null;
-		var recordReceipt = Function0.of(() -> receiptWrite.recordReceipt(receiptEntity));
-		var success = validJson && recordReceipt.get() != null;
-		return Collections.firstTrueEagerStateOf(List.of(
-						Pair.of(success, new SimpleHTTPResponse(HttpStatus.CREATED, Map.of("message", "created!"))),
-						Pair.of(validJson, Database.couldNotWrite()),
-						Pair.of(true, Receipt.mapJsonResultsToReceiptResponse(validationOutcome))));
+		return Validation.validateJsonSchemaFrom(jsonFile).apply(receipt).get();
 	}
 
-	public void validateItems(Path file, JsonNode items) {
+	public List<Pair<String, String>> validateItems(Path file, @NotNull JsonNode items) {
 		var validateItem = Validation.validateJsonSchemaFrom(file);
-		for (JsonNode item : items) {
-			var validationResult = validateItem.apply(item);
-		}
-		// invalid json file
+		return StreamSupport
+						.stream(items.spliterator(), true)
+						.map(validateItem)
+						.map(Lazy::get)
+						.toList();
 	}
 
-	public SimpleHTTPResponse getItemValidationResult(@RequestBody JsonNode receipt) {
-		var jsonFile = Paths.get("src/main/java/com/example/receiptprocessor/data/schemas/items.json");
+	public List<Pair<String, String>> validateItems(@RequestBody @NotNull JsonNode receipt) {
+		var jsonFile = Paths.get("src/main/java/com/example/receiptprocessor/data/schemas/item.json");
 		var items = receipt.get("items");
-//		var validationResultsForItems = validateItems(jsonFile, items);
-//		var validationResultOrFailure = Validation.validateJsonSchema(jsonFile, receipt);
-//		var validationOptions = Receipt.mapJsonResultsToReceiptResponse(validationResultOrFailure);
-//		var validationResult = Collections.firstTrueStateOf(validationOptions).get();
-//		var validJson = validationResult.statusCode() == HttpStatus.CREATED;
-//		var receiptEntity = validJson? ReceiptWrite.hydrate(receipt) : null;
-//		var recordReceipt = Function0.of(() -> receiptWrite.recordReceipt(receiptEntity));
-//		var success = validJson && recordReceipt.get() != null;
-//		return Collections.firstTrueEagerStateOf(List.of(
-//						Pair.of(success || !validJson, validationResult),
-//						Pair.of(true, Database.couldNotWrite())));
+		var hasItems = items != null;
+		var validationOutcomes = hasItems? validateItems(jsonFile, items) : null;
+		assert validationOutcomes != null;
+		return validationOutcomes.stream()
+						.filter(validationResult -> !validationResult.getFirst().equals(Json.MATCHED_SCHEMA))
+						.toList();
+
 	}
 
 	@PostMapping("/process")
 	public ResponseEntity<Map<String, String>> recordReceipt(@RequestBody JsonNode receipt) {
-//		var receiptResponse = validateReceipt(receipt); // todo: get query to make, bundle w/ items
-//		var itemResponses = getItemValidationResult(receipt); // todo: get query to make, bundle w/ receipt
-//		// todo: try saving receipt and items, revert if either fail
-//		return ResponseEntity.status(responseResult.statusCode()).body(responseResult.body());
+		Pair<String, String> receiptOutcomes = validateReceipt(receipt);
+		var validReceipt = receiptOutcomes.getFirst().equals(Json.MATCHED_SCHEMA);
+		var invalidItemOutcomes = validateItems(receipt);
+		List<Lazy<Object>> itemQueries = invalidItemOutcomes.isEmpty()?
+						getItemQueries(receipt.get("items")) :
+						List.of();
+		var invalidData = validReceipt?
+						invalidItemOutcomes :
+						Collections.add(invalidItemOutcomes).apply(receiptOutcomes);
+		var queriesToRun = (validReceipt && invalidItemOutcomes.isEmpty()) ?
+						Collections.add(itemQueries).apply(getReceiptQuery(receipt)) :
+						itemQueries;
+		if(!queriesToRun.isEmpty()) {
+			//noinspection ResultOfMethodCallIgnored
+			queriesToRun.stream().map(Lazy::get);
+		}
+		var responseInfo = invalidData.isEmpty()?
+						new SimpleHTTPResponse(HttpStatus.CREATED, Map.of("message", "receipt stored!")) :
+						errorState(invalidData);
+
+		return ResponseEntity.status(responseInfo.statusCode()).body(responseInfo.body());
+	}
+	SimpleHTTPResponse errorState(@NotNull List<Pair<String, String>> invalidData) {
+		var errors = Map.of("errors", invalidData.toString());
+		return new SimpleHTTPResponse(HttpStatus.BAD_REQUEST, errors);
 	}
 
 	@GetMapping("/{id}/points")
