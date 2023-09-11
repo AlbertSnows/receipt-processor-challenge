c38bd99270cfa5ce4e62985d6e3bfbccdac8b090
Albert Snow <ajsnow2012@gmail.com>
2023-09-11 17:26:51 -0500

    defining states

--- src/main/java/com/example/receiptprocessor/data/states/Points.java
+++ src/main/java/com/example/receiptprocessor/data/states/Points.java
@@ -1,93 +1,76 @@
 package com.example.receiptprocessor.data.states;
 
+import com.example.receiptprocessor.data.entities.Item;
+import com.example.receiptprocessor.data.entities.Receipt;
 import com.example.receiptprocessor.data.records.ItemDescLengthAndPrice;
-import com.example.receiptprocessor.data.records.SimpleHTTPResponse;
 import com.example.receiptprocessor.utility.Shorthand;
+import io.vavr.Function0;
 import io.vavr.Lazy;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.util.Pair;
-import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Map;
+import java.util.List;
 
 public class Points {
-	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-	retailerNameCount(com.example.receiptprocessor.data.entities.@NotNull Receipt receipt) {
-		var retailerPointNames = receipt.getRetailer().length();
-
-		return Shorthand.makeLazyStatePair(
-						() -> receipt == null,
-						() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-										Map.of("error", "No receipt associated with provided id.")));
-	}
-	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-	roundTotal(com.example.receiptprocessor.data.entities.Receipt receipt) {
-		var totalIsRound = retailerPurchaseTotal.scale() == 0;
-		return Shorthand.makeLazyStatePair(
-						() -> receipt == null,
-						() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-										Map.of("error", "No receipt associated with provided id.")));
+	private Points() {
+		throw new IllegalStateException("Utility class");
 	}
 
-	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-	quarterFractional(com.example.receiptprocessor.data.entities.Receipt receipt) {
-		var quarterFractional = totalIsRound
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	retailerNameCount(Receipt receipt) {
+		return Shorthand.makeLazyStatePair(
+						() -> receipt != null,
+						() -> receipt.getRetailer().length());
+	}
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	roundTotal(BigDecimal retailerPurchaseTotal) {
+		return Shorthand.makeLazyStatePair(
+						() -> retailerPurchaseTotal.scale() == 0,
+						() -> 50);
+	}
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	quarterFractional(@NotNull BigDecimal retailerPurchaseTotal) {
+		Function0<Boolean> quarterFractional = () -> retailerPurchaseTotal.scale() == 0
 						|| retailerPurchaseTotal.remainder(new BigDecimal("0.25")).equals(BigDecimal.ZERO);
-
-		return Shorthand.makeLazyStatePair(
-						() -> receipt == null,
-						() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-										Map.of("error", "No receipt associated with provided id.")));
+		return Shorthand.makeLazyStatePair(quarterFractional, () -> 25);
 	}
 
-public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-				pointsPerTwoItems(com.example.receiptprocessor.data.entities.Receipt receipt) {
-	var haveRelevantItems = receiptItems.size() >= 2;
-
-				return Shorthand.makeLazyStatePair(
-				() -> receipt == null,
-				() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-				Map.of("error", "No receipt associated with provided id.")));
-				}
-
-public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-				itemPricePointsFromItemDescription(com.example.receiptprocessor.data.entities.Receipt receipt) {
-	//todo: pretrim descriptions
-	var relevantItemPrices = receiptItems.stream()
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	pointsPerTwoItems(List<Item> receiptItems) {
+		return Shorthand.makeLazyStatePair(
+				() -> receiptItems.size() >= 2,
+				() -> 5 * (receiptItems.size() / 2));
+	}
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	itemPricePointsFromItemDescription(@NotNull List<Item> receiptItems) {
+		//todo: pretrim descriptions
+		var relevantItemPrices = receiptItems.stream()
 					.map(item -> new ItemDescLengthAndPrice(item.getShortDescription().length(), item.getPrice()))
-					.filter(itemRecord -> itemRecord.length() % 3 == 0);
-	//todo: pass stream to success state (if not relevant)
-//						.map(itemRecord -> itemRecord.price().multiply(BigDecimal.valueOf(0.2)))
-//						.map(BigDecimal::scale)
-//						.mapToInt(Integer::intValue)
-//						.sum();
-				return Shorthand.makeLazyStatePair(
-				() -> receipt == null,
-				() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-				Map.of("error", "No receipt associated with provided id.")));
-				}
-
-public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-				oddPurchaseDate(com.example.receiptprocessor.data.entities.Receipt receipt) {
-	var purchaseMonthDay = purchaseDateTime.getDayOfMonth();
-	var oddDay = purchaseMonthDay % 2 == 1;
-				return Shorthand.makeLazyStatePair(
-				() -> receipt == null,
-				() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-				Map.of("error", "No receipt associated with provided id.")));
-				}
-
-public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<SimpleHTTPResponse>>
-				timeBetweenTwoAndFour(com.example.receiptprocessor.data.entities.Receipt receipt) {
-	var purchaseTime = purchaseDateTime.toLocalTime();
-	var isBetweenTwoAndFour = purchaseTime.isAfter(LocalTime.of(14, 0))
-					&& purchaseTime.isBefore(LocalTime.of(16, 0));
-
-				return Shorthand.makeLazyStatePair(
-				() -> receipt == null,
-				() -> new SimpleHTTPResponse(HttpStatus.NOT_FOUND,
-				Map.of("error", "No receipt associated with provided id.")));
-				}
+					.filter(itemRecord -> itemRecord.length() % 3 == 0).toList();
+		return Shorthand.makeLazyStatePair(
+		() -> !relevantItemPrices.isEmpty(),
+		() -> relevantItemPrices.stream()
+						.map(itemRecord -> itemRecord.price().multiply(BigDecimal.valueOf(0.2)))
+						.map(BigDecimal::scale)
+						.mapToInt(Integer::intValue)
+						.sum());
+	}
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	oddPurchaseDate(@NotNull LocalDateTime purchaseDateTime) {
+		var purchaseMonthDay = purchaseDateTime.getDayOfMonth();
+		return Shorthand.makeLazyStatePair(
+						() -> purchaseMonthDay % 2 == 1,
+						() -> 6);
+	}
+	public static @org.jetbrains.annotations.NotNull Pair<Lazy<Boolean>, Lazy<Integer>>
+	timeBetweenTwoAndFour(@NotNull LocalDateTime purchaseDateTime) {
+		var purchaseTime = purchaseDateTime.toLocalTime();
+		return Shorthand.makeLazyStatePair(
+						() -> purchaseTime.isAfter(LocalTime.of(14, 0))
+										&& purchaseTime.isBefore(LocalTime.of(16, 0)),
+						() -> 10);
+	}
 }
